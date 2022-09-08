3fa470a370fec304f9798c6bdc751d43bb3f05ee
jair escobar <jaires1823@gmail.com>
2022-09-08 14:16:10 -0500

    He creado una base de datos

---
+++ datos.sql
@@ -0,0 +1,4 @@
+create database Miplataforma;
+use Miplataforma;
+
+create table persona(idpersona int,nombres varchar(20),apellidos varchar(20),cedula varchar(10),idgenero int,fechanacimiento date,idnacionalidad int,idetnia int,estadocivil int);
